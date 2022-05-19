cae3c04ca1fbcedfdcacb95ad068ab482f45746b
tmubasshera <mubassherat@gmail.com>
2022-05-19 11:08:43 +0800

    First Commit

---
+++ event_act/android/app/src/main/kotlin/com/example/event_act/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.event_act
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ event_act/lib/models/user.dart
@@ -0,0 +1,11 @@
+class User {
+  String? id;
+  String? email;
+  String? password;
+  bool? isAdmin;
+
+  User({this.id});
+
+  //User({ this.id,  this.email,  this.password,  this.isAdmin });
+
+}
