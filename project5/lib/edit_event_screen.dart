76d84625b4d944a00d0bd92895a6b9202398e34f
EyadMohgoub <105339030+EyadMohgoub@users.noreply.github.com>
2022-06-01 16:02:29 +0300

    Update edit_event_screen.dart

--- project5/lib/edit_event_screen.dart
+++ project5/lib/edit_event_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:project5/home_screen.dart';
 import 'Models/event.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class EditEvent extends StatefulWidget {
   const EditEvent({Key? key, required this.event}) : super(key: key);
@@ -10,12 +12,23 @@ class EditEvent extends StatefulWidget {
   final Event event;
 
   @override
-  State<EditEvent> createState() => _EditEventState();
+  State<EditEvent> createState() {
+    return _EditEventState(this.event);
+  }
 }
 
 class _EditEventState extends State<EditEvent> {
-  TextEditingController _dateController = TextEditingController();
-  TextEditingController _descController = TextEditingController();
+  final Event event;
+
+  _EditEventState(this.event);
+
+  
+
+
+  late TextEditingController _dateController = TextEditingController(text: event.date);
+  late TextEditingController _descController = TextEditingController(text: event.desc);
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +49,7 @@ class _EditEventState extends State<EditEvent> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            Text("event.title", style: TextStyle(
+            Text(event.name, style: const TextStyle(
             color: Colors.black,
             fontSize: 44.0,
             fontWeight: FontWeight.bold,
@@ -48,7 +61,7 @@ class _EditEventState extends State<EditEvent> {
             controller: _dateController,
             keyboardType: TextInputType.emailAddress,
             decoration: const InputDecoration(
-              hintText: "User Email",
+              hintText: "Date of Event",
               prefixIcon: Icon(Icons.mail, color: Colors.black,),
             ),
           ),
@@ -56,10 +69,39 @@ class _EditEventState extends State<EditEvent> {
           TextField(
             controller: _descController,
             decoration: const InputDecoration(
-              hintText: "Password",
+              hintText: "Description",
               prefixIcon: Icon(Icons.lock, color: Colors.black,),
             ),
           ),
+
+          const SizedBox(height: 88.0,),
+          
+          Container(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: const Color(0xFF0069FE),
+              elevation: 0.0,
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+              onPressed: () async {
+                
+                FirebaseFirestore.instance.collection('Events').doc(event.name).update({
+                  "Date" : _dateController.text,
+                  "Description": _descController.text,
+                  "Name": event.name, 
+                }).then((value) => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>OrgHomeScreen())));
+                
+            
+                  
+              },
+              child: const Text("Save", style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),),
+            ),
+          ),
+
+
           ],
         ),
         ),
